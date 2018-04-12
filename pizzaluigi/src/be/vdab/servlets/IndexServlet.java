46b61ab04196129ea3f6559d54a1f9a70565bacc
jonas Ketelslegers <jonas.ketelslegers@gmail.com>
2018-04-12 09:50:43 +0200

    Newest PIzza Luigi, Chapter 12

--- pizzaluigi/src/be/vdab/servlets/IndexServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IndexServlet.java
@@ -1,8 +1,10 @@
 package be.vdab.servlets;
 
+import be.vdab.entities.Begroeting;
+import be.vdab.entities.Adres;
+import be.vdab.entities.Persoon;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,26 +12,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /**
  * Servlet implementation class IndexServlet
  */
 @WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();
-		out.println("<!doctype html>");
-		out.println("html lang='nl'><head>");
-		out.println("<title>Pizza Luigi</title></head>");
-		out.println("<body><h1>");
-		int uur = LocalDateTime.now().getHour();
-		out.print(uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
-		out.println("</h1></body></html>");
+			request.setAttribute("begroeting", new Begroeting());
+			request.setAttribute("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true,
+					new Adres("Grote Markt", "3", 9700, "Oudenaarde")));
+			request.getRequestDispatcher(VIEW).forward(request, response);
+			
 	}
-
 }
