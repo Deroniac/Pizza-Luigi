f7cf2856e789101a4d6ab2696919f766358cddc4
jonas Ketelslegers <jonas.ketelslegers@gmail.com>
2018-04-20 15:55:31 +0200

    Chapter 22

--- pizzaluigi/src/be/vdab/servlets/IdentificatieServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IdentificatieServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class IdentificatieServlet
@@ -18,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 public class IdentificatieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/identificatie.jsp";
-	private static final int COOKIE_MAXIMUM_LEEFTIJD = 60 /* seconden */ * 30 /* minuten */;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -31,15 +31,15 @@ public class IdentificatieServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		if (request.getCookies() != null) {
-			for (Cookie cookie : request.getCookies()) {
-				if ("gebruikersnaam".equals(cookie.getName())) {
-					request.setAttribute("gebruikersnaam", URLDecoder.decode(cookie.getValue(), "UTF-8"));
-					break;
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			String gebruikersnaam = (String)session.getAttribute("gebruikersnaam");
+				if (gebruikersnaam != null) {
+					request.setAttribute("gebruikersnaam", gebruikersnaam);
 				}
-			}
 		}
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
@@ -47,13 +47,14 @@ public class IdentificatieServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
-		Cookie cookie = new Cookie("gebruikersnaam", URLEncoder.encode(request.getParameter("gebruikersnaam"), "UTF-8"));
-		cookie.setMaxAge(COOKIE_MAXIMUM_LEEFTIJD);
-		response.addCookie(cookie);
-		response.sendRedirect(request.getRequestURI());
+		HttpSession session = request.getSession();
+		session.setAttribute("gebruikersnaam", request.getParameter("gebruikersnaam"));
+		//redirect naar huidige URL
+		response.sendRedirect(response.encodeRedirectUrl(request.getRequestURI()));
 	}
 
 }
